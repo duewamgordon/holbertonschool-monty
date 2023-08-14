da10576c0aa8182ad87c14a3abec051c4f981e23
root <duewamgordon@gmail.com>
2023-08-14 12:40:02 -0700

    montycode2

--- pushpall.c
+++ pushpall.c
@@ -41,21 +41,3 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 	arguments->stack_len += 1;
 }
-
-/**
- * pall - Prints all values on the stack.
- * @stack: Pointer to the top of the stack
- * @line_number: line number
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-	(void)line_number;
-	stack_t *current = *stack;
-    
-
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
-}
