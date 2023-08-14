54cc7c3ba81f2acd0e0a291a7286f65733a8f304
root <duewamgordon@gmail.com>
2023-08-14 14:44:09 -0700

    montycodes5

--- function_2.c
+++ function_2.c
@@ -5,8 +5,7 @@
  */
 void handle_invalid_instruction(void)
 {
-    dprintf(2, "L%d: unknown instruction  %s\n", 
-		    arguments->line_num, arguments->tok[0]);
+    dprintf(2, "L%d: unknown instructions %s\n", arguments->line_number, arguments->tok[0]);
     close_stream();
     free_tok();
     free_args();
