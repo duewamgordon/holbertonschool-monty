eb941315bc09cf0faffdcb4728853b978c92fe19
root <duewamgordon@gmail.com>
2023-08-14 21:08:37 -0700

    montycode9

--- token_break.c
+++ token_break.c
@@ -7,21 +7,23 @@
 void token_break(void)
 {
 	int d = 0;
-	char *delim = "\n", *token = NULL, *lncpy = NULL;
+	char *delims = " \n", *token = NULL, *lncpy = NULL;
 
 	lncpy = malloc(sizeof(char) * (strlen(arguments->text_line) + 1));
 	strcpy(lncpy, arguments->text_line);
 	arguments->num_tok = 0;
-	token = strtok(lncpy, delim);
+	token = strtok(lncpy, delims);
 	while (token)
 	{
+		printf(" %s\n", token);
+
 		arguments->num_tok += 1;
-		token = strtok(NULL, delim);
+		token = strtok(NULL, delims);
 	}
 
 	arguments->tok = malloc(sizeof(char *) * (arguments->num_tok + 1));
 	strcpy(lncpy, arguments->text_line);
-	token = strtok(lncpy, delim);
+	token = strtok(lncpy, delims);
 
 	while (token)
 	{
@@ -29,7 +31,7 @@ void token_break(void)
 		if(arguments->tok[d] == NULL)
 			end_malloc();
 		strcpy(arguments->tok[d], token);
-		token = strtok(NULL, delim);
+		token = strtok(NULL, delims);
 		d++;
 	}
 	arguments->tok[d] = NULL;
